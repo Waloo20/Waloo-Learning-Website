66c30d91cf428af32d1abb217f6e64d60ee6dad9
Waloo20 <126532058+Waloo20@users.noreply.github.com>
2023-03-22 12:09:09 +0000

    Added Html and CSS, new pages, and content.

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from data import Data
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def home():
+  return render_template('home.html',
+                        data = Data)
 
+@app.route("/blog")
+def blog():
+  return render_template('blog.html')
+  
+@app.route("/credentials")
+def credentials():
+  return render_template('credentials.html')
+  
+@app.route("/CardPredictor")
+def CardPredictor():
+  return render_template('CardPredictor.html')
 
+@app.route("/WebsiteProject")
+def WebsiteProject():
+  return render_template('WebsiteProject.html')
+
+@app.route("/CactusProject")
+def CactusProject():
+  return render_template('CactusProject.html')
+
+@app.route("/Aboutme")
+def Aboutme():
+  return render_template('Aboutme.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ data.py
@@ -0,0 +1,1 @@
+Data = [{"Head":"Education", "Content":"The University of Southern Mississippi\nMicrobiology Major\nComputer Science Major"}, {"Head":"Expertise", "Content":"Polymerase Chain Reaction\nTransmissable vector preperation\n Gel electrophorsis\nPython"}]
