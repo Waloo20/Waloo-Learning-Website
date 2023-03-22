eed6656479b7fd14cd233d2ab6c13158a3d135ec
Waloo20 <126532058+Waloo20@users.noreply.github.com>
2023-03-22 13:11:11 +0000

    updated some content, and removed some html features

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, send_from_directory
 from data import Data
-
 app = Flask(__name__)
 
 
--- data.py
+++ data.py
@@ -1,1 +1,2 @@
+import random
 Data = [{"Head":"Education", "Content":"The University of Southern Mississippi\nMicrobiology Major\nComputer Science Major"}, {"Head":"Expertise", "Content":"Polymerase Chain Reaction\nTransmissable vector preperation\n Gel electrophorsis\nPython"}]
